4d0fb04e72f7dc121bb4adfe955c5dbec281c5ff
pchaos <drifthua@gmail.com>
2023-06-29 17:45:41 +0800

    update

--- plugin/lua_format.lua
+++ plugin/lua_format.lua
@@ -1,11 +1,12 @@
--- Last Modified: 2023-06-29 17:41:11
+-- Last Modified: 2023-06-29 17:45:26
 if vim.api.nvim_eval('exists("g:loaded_vim_lua_formatter")') ~= 0 then
   return
 end
 
+local api = vim.api
 local cmd=vim.cmd
 local o=vim.o
-
+pp
 local save_cpo = o.cpo -- save user coptions
 o.cpo = vim.o.cpo .. 'vim' -- reset them to defaults
 
@@ -27,11 +28,18 @@ _G.loaded_vim_lua_formatter= 1
 --   autocmd FileType lua nnoremap <buffer> <c-l> :call lua_format()<cr>
 --   autocmd BufWritePre *.lua call lua_format()
 -- ]])
-cmd([[
+-- cmd([[
+--   autocmd FileType lua nnoremap <buffer> <c-l> :call lua_format()<cr>
+-- ]])
+
+-- cmd([[
+--   autocmd BufWritePre *.lua call lua_format()
+-- ]])
+api.nvim_command([[
   autocmd FileType lua nnoremap <buffer> <c-l> :call lua_format()<cr>
 ]])
 
-cmd([[
+api.nvim_command([[
   autocmd BufWritePre *.lua call lua_format()
 ]])
 
