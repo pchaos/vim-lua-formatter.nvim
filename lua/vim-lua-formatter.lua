f1d8570668e5d7b6c6a2c42026f47a4830d16463
pchaos <drifthua@gmail.com>
2023-06-29 19:15:53 +0800

    fix typo

--- lua/vim-lua-formatter.lua
+++ lua/vim-lua-formatter.lua
@@ -1,4 +1,4 @@
--- Last Modified: 2023-06-29 19:13:26
+-- Last Modified: 2023-06-29 19:15:38
 
 local cmd = vim.cmd -- execute Vim commands
 local exec = vim.api.nvim_exec -- execute Vimscript
@@ -58,12 +58,12 @@ function lua_format_format()
     local config_file = fn.findfile(".lua-format", ".;")
     if config_file ~= "" then flags = flags .. " -c " .. config_file
   else
-local pluginDirectory = GetPluginDirectory()
-print("插件所在目录：" .. pluginDirectory)
-    config_file = fn.findfile(".lua-format.default", pluginDirectory)
-if config_file ~= "" then f
-flags = flags .. " -c " .. config_file
-    end
+    local pluginDirectory = GetPluginDirectory()
+    print("插件所在目录：" .. pluginDirectory)
+        config_file = fn.findfile(".lua-format.default", pluginDirectory)
+    if config_file ~= "" then
+    flags = flags .. " -c " .. config_file
+        end
 
   end
   -- todo 如果没有找到".lua-format"文件，则使用插件提供的默认配置文件：".lua-format.default"
