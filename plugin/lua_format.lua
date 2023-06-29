e5a43485aff48b34e415a7c318c950c34bcd5b92
pchaos <drifthua@gmail.com>
2023-06-29 16:44:13 +0800

    update

--- plugin/lua_format.lua
+++ plugin/lua_format.lua
@@ -1,16 +1,17 @@
--- Last Modified: 2023-06-29 15:44:55
+-- Last Modified: 2023-06-29 16:40:32
+--
 local ok, lua_formatter = pcall(require, 'vim-lua-formatter')
 if not ok then
     -- not loaded
     print("function lua_format_format not found.")
     require("vim-lua-formatter")
-  function lua_format() lua_format_format() end
+  return
 else
-    return
+    print("define lua_format")
+  function lua_format() lua_format_format() end
 end
 
-
 vim.cmd([[
-  autocmd FileType lua nnoremap <buffer> <c-k> :call LuaFormat()<cr>
+  autocmd FileType lua nnoremap <buffer> <c-l> :call LuaFormat()<cr>
   autocmd BufWritePre *.lua call LuaFormat()
 ]])
