140820cc1f267c391982ed2a48d95705de7a13bd
pchaos <drifthua@gmail.com>
2023-06-29 11:09:19 +0800

    add message not found function lua_format_format

--- plugin/lua_format.lua
+++ plugin/lua_format.lua
@@ -1,4 +1,12 @@
-require("vim-lua-formatter")
+-- Last Modified: 2023-06-29 11:07:39
+local ok, _ = pcall(require, 'lua_format_format')
+if not ok then
+    -- not loaded
+    print("function lua_format_format not found.")
+    require("vim-lua-formatter")
+else
+    return
+end
 
 function lua_format() lua_format_format() end
 
