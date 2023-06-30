32f14c57aab311bfb93278ce7a5686392e891c5d
pchaos <drifthua@gmail.com>
2023-06-30 12:34:03 +0800

    fix lwindow

--- lua/vim-lua-formatter.lua
+++ lua/vim-lua-formatter.lua
@@ -1,4 +1,4 @@
--- Last Modified: 2023-06-30 11:58:30
+-- Last Modified: 2023-06-30 12:35:49
 
 local cmd = vim.cmd -- execute Vim commands
 local exec = vim.api.nvim_exec -- execute Vimscript
@@ -8,141 +8,136 @@ local opt = vim.opt -- global/buffer/windows-scoped options
 local api = vim.api
 
 local function GetPluginDirectory()
-    local scriptPath = debug.getinfo(1, 'S').source:sub(2)
-    local pluginDirectory = vim.fn.fnamemodify(scriptPath, ':h')
-    return pluginDirectory
+  local scriptPath = debug.getinfo(1, 'S').source:sub(2)
+  local pluginDirectory = vim.fn.fnamemodify(scriptPath, ':h')
+  return pluginDirectory
 end
 
 local function printFileContent(filePath)
-    local file = io.open(filePath, "r")
-    if file then
-        local content = file:read("*a")
-        io.close(file)
-        print(content)
-    else
-        print("无法打开文件：" .. filePath)
-    end
+  local file = io.open(filePath, "r")
+  if file then
+    local content = file:read("*a")
+    io.close(file)
+    print(content)
+  else
+    print("无法打开文件：" .. filePath)
+  end
 end
 
 local function isExecutableExists(executable)
-    local command = "command -v " .. executable
+  local command = "command -v " .. executable
 
-    -- 执行命令并获取结果
-    local handle = io.popen(command)
-    local result = handle:read("*a")
-    handle:close()
+  -- 执行命令并获取结果
+  local handle = io.popen(command)
+  local result = handle:read("*a")
+  handle:close()
 
-    -- 判断结果是否为空
-    if result ~= "" then
-        return true
-    else
-        return false
-    end
+  -- 判断结果是否为空
+  if result ~= "" then
+    return true
+  else
+    return false
+  end
 end
 
-function printValue(value)
-    for k, v in pairs(value) do
-        print(k, v)
-    end
-end
+function printValue(value) for k, v in pairs(value) do print(k, v) end end
 
 local function lua_format_CopyDiffToBuffer(input, output, bufname)
-    -- prevent out of range in cickle
-    local min_len = math.min(#input, #output)
+  -- prevent out of range in cickle
+  local min_len = math.min(#input, #output)
 
-    -- copy all lines that were changed
-    for i = 1, min_len do
-        local output_line = output[i]
-        local input_line = input[i]
-        if input_line ~= output_line then
-            api.nvim_buf_set_lines(0, i, i, false, {output_line})
-        end
+  -- copy all lines that were changed
+  for i = 1, min_len do
+    local output_line = output[i]
+    local input_line = input[i]
+    if input_line ~= output_line then api.nvim_buf_set_lines(0, i, i, false, { output_line }) end
+  end
+
+  -- handle all lines that were in range
+  if #input ~= #output then
+    if min_len == #output then
+      -- remove all extra lines from input
+      api.nvim_buf_set_lines(0, min_len + 1, -1, false, {})
+    else
+      -- append all extra lines from output
+      local extra_lines = {}
+      for j = min_len + 1, #output do table.insert(extra_lines, output[j]) end
+      api.nvim_buf_set_lines(0, -1, -1, true, extra_lines)
     end
+  end
 
-    -- handle all lines that were in range
-    if #input ~= #output then
-        if min_len == #output then
-            -- remove all extra lines from input
-            api.nvim_buf_set_lines(0, min_len + 1, -1, false, {})
-        else
-            -- append all extra lines from output
-            local extra_lines = {}
-            for j = min_len + 1, #output do
-                table.insert(extra_lines, output[j])
-            end
-            api.nvim_buf_set_lines(0, -1, -1, true, extra_lines)
-        end
-    end
-
-    -- redraw windows to prevent invalid data display
-    api.nvim_call_function("redraw!", {})
-  -- cmd("redraw!")
+  -- redraw windows to prevent invalid data display
+  -- api.nvim_call_function("redraw!", {})
+  cmd("redraw!")
 end
 
 function lua_format_format()
-    local input = api.nvim_buf_get_lines(0, 0, -1, false)
+  local input = api.nvim_buf_get_lines(0, 0, -1, false)
 
-    -- create a temporary file to capture error messages
-    local error_file = fn.tempname()
+  -- create a temporary file to capture error messages
+  local error_file = fn.tempname()
 
-    local flags = " -i "
+  local flags = " -i "
 
-    -- use config file for formatting if available
-    local config_file = fn.findfile(".lua-format", ".;")
-    if config_file ~= "" then 
-      flags = flags .. " -c " .. config_file
-    else
+  -- use config file for formatting if available
+  local config_file = fn.findfile(".lua-format", ".;")
+  if config_file ~= "" then
+    flags = flags .. " -c " .. config_file
+  else
     -- todo 如果没有找到".lua-format"文件，则使用插件提供的默认配置文件：".lua-format.default"
     local pluginDirectory = GetPluginDirectory()
     print("插件所在目录：" .. pluginDirectory)
-        config_file = fn.findfile(".lua-format.default", "pluginDirectory;pluginDirectory/**")
-    if config_file ~= "" then
-    flags = flags .. " -c " .. config_file
-    end
+    config_file = fn.findfile(".lua-format.default", "pluginDirectory;pluginDirectory/**")
+    if config_file ~= "" then flags = flags .. " -c " .. config_file end
 
   end
-    print(config_file,"flag:" .. flags)
-    print("error_file:" .. error_file)
-    printFileContent(config_file)
+  print(config_file, "flag:" .. flags)
+  print("error_file:" .. error_file)
+  printFileContent(config_file)
 
-local executableExists = isExecutableExists("lua-format")
-if executableExists then
+  local executableExists = isExecutableExists("lua-format")
+  if executableExists then
     print("lua-format 可执行文件存在")
-
+    --
+    -- 获取当前活动窗口
+    local current_win = vim.api.nvim_get_current_win()
     local command = "lua-format" .. flags .. " 2> " .. error_file
-    local output= fn.systemlist(command, input)
+    local output = fn.systemlist(command, input)
 
-  -- print("exit_code:" .. exit_code)
-  print("output:")
-  -- printValue(output)
+    -- print("exit_code:" .. exit_code)
+    print("output:")
+    -- printValue(output)
     if #output > 0 then -- all right
-        lua_format_CopyDiffToBuffer(input, output, fn.bufname("%"))
+      lua_format_CopyDiffToBuffer(input, output, fn.bufname("%"))
 
-        -- clear message buffer
-        api.nvim_call_function('lexpr', {""})
-        api.nvim_call_function('lwindow', {})
-        -- cmd("lexpr \"\"")
-        -- cmd("lwindow")
+      -- clear message buffer
+      api.nvim_call_function('lexpr', { "" })
+      -- api.nvim_call_function('lwindow', {})
+      vim.api.nvim_set_current_win(current_win)
+      -- cmd("lexpr \"\"")
+      -- cmd("lwindow")
     else -- we got an error
-        local errors = fn.readfile(error_file)
+      local errors = fn.readfile(error_file)
 
-        -- insert filename of current buffer in front of the list. Needed for errorformat
-        local source_file = fn.bufname("%")
-        table.insert(errors, 1, source_file)
+      -- insert filename of current buffer in front of the list. Needed for errorformat
+      local source_file = fn.bufname("%")
+      table.insert(errors, 1, source_file)
 
-        opt.efm = "%+P%f,line\\ %l:%c\\ %m,%-Q"
-        -- for k, v in pairs(errors) do
-        --     print(k, v)
-        -- end
-        api.nvim_call_function('setloclist', {0, errors, 'r'})
-        api.nvim_call_function('lwindow', {5})
-        -- cmd("lwindow 5")
+      opt.efm = "%+P%f,line\\ %l:%c\\ %m,%-Q"
+      -- for k, v in pairs(errors) do
+      --     print(k, v)
+      -- end
+      api.nvim_call_function('setloclist', { 0, errors, 'r' })
+      -- api.nvim_call_function('lwindow', {5})
+      -- 切换到窗口编号 5
+      vim.api.nvim_set_current_win(5)
+      -- cmd("lwindow 5")
     end
   else
     print("lua-format 可执行文件不存在")
     return
-end
+  end
 
-    -- delete the temporary file
-    fn.delete(error_file)
+  -- delete the temporary file
+  fn.delete(error_file)
 end
